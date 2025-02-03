40b9c2e4158a45916914eb442a396cb680491554
ShariarNiaj05 <shariarn85@gmail.com>
2025-02-03 22:10:28 +0600

    server setup

--- src/index.ts
+++ src/index.ts
@@ -30,3 +30,16 @@ const books = [
     author: "Paul Auster",
   },
 ];
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
