a3b2fc91947461c609f5f78553b605460834c041
ShariarNiaj05 <shariarn85@gmail.com>
2025-02-04 22:46:27 +0600

    query call

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { db } from "./db";
+import { db } from "./db.js";
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
