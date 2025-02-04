82ec4a9c1b9d63519e4e3a7b5eb4933582d7a2f8
ShariarNiaj05 <shariarn85@gmail.com>
2025-02-04 22:32:27 +0600

    products list added

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,25 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = `#graphql
-  
+  type Query {
+id: ID!
+name: String
+description: String
+price: Float
+quantity: Int
+onStock: Boolean
+categoryId: ID!
+
+  id: "2a089dca-d882-4305-9e25-d1dfeb93fd12",
+    name: "Basketball",
+    image: "basketball-image.jpg",
+    description:
+      "An official size basketball for both indoor and outdoor play.",
+    price: 29.99,
+    quantity: 30,
+    onStock: true,
+    categoryId: "4f7f61e5-96c2-445d-80fb-79f58e3d061b",
+  }
 
 `;
 
