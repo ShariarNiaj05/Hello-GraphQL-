049b1eeddf26e2cb4bcec5ad3327720ef958f8bd
ShariarNiaj05 <shariarn85@gmail.com>
2025-02-04 22:33:06 +0600

    query setup

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,14 @@ const typeDefs = `#graphql
   type Query {
 id: ID!
 name: String
+image: String
 description: String
 price: Float
 quantity: Int
 onStock: Boolean
 categoryId: ID!
 
-  id: "2a089dca-d882-4305-9e25-d1dfeb93fd12",
-    name: "Basketball",
-    image: "basketball-image.jpg",
-    description:
-      "An official size basketball for both indoor and outdoor play.",
-    price: 29.99,
-    quantity: 30,
-    onStock: true,
+
     categoryId: "4f7f61e5-96c2-445d-80fb-79f58e3d061b",
   }
 
