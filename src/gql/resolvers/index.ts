17a367d73b4e2ed6d9a072c844bfb2884bca4bc8
ShariarNiaj05 <shariarn85@gmail.com>
2025-02-10 22:11:55 +0600

    query cleanup

--- src/gql/resolvers/index.ts
+++ src/gql/resolvers/index.ts
@@ -6,7 +6,6 @@ export const resolvers = {
   Query: {
     products: () => db.products,
     product: (parent: any, args: { productId: string }, context: any) => {
-      console.log({ parent }, { args }, { context });
       const result = db.products.find((pd) => pd.id === args.productId);
       return result;
     },
