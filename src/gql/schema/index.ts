2c73b29f41e062f3abcb4f31f3dc486a7d0c40eb
ShariarNiaj05 <shariarn85@gmail.com>
2025-02-10 22:14:21 +0600

    completed gql basic

--- src/gql/schema/index.ts
+++ src/gql/schema/index.ts
@@ -1,6 +1,5 @@
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
-// your data.
 export const typeDefs = `#graphql
   type Product {
 id: ID!
